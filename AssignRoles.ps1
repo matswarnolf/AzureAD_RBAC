7be8274dee1075ed110b8fa256f072932d73df9b
Mats Warnolf <mats@mwab.onmicrosoft.com>
2019-10-03 09:41:36 +0200

    done AssignRoles

--- AssignRoles.ps1
+++ AssignRoles.ps1
@@ -1,16 +1,23 @@
+# Det här skriptet skapar en ny resourcegroup och en användare som tilldelas två roller:
+#  - Reader på subscriptionnivå för att låta användaren navigera 
+#  - Contributor på ResourceGroup-nivå för att låta användaren jobba med vår resource group
+
+# TODO Lägg till rätt subscription ID i variablen SubscriptionScope
+
 # Skapa användare ------------------------------------------------------
 $UserPrincipalName = "rbacdemouser@example.com"
 $ResourceGroupName = "RBAC_DemoGroup"
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$PasswordProfile.Password = "Password"
+$PasswordProfile.Password = "MyStupidDemoPa55w0rd"
+$SubscriptionScope = "/subscriptions/00000000-0000-0000-0000-000000000000" # ToDo Ändra här
+
 $UserParamameters = @{
     DisplayName       = "RBAC Demo User";
     PasswordProfile   = $PasswordProfile;
     UserPrincipalName = $UserPrincipalName;
     AccountEnabled    = $true;
-    MailNickName      = "rbacuser"
+    MailNickName      = "rbacdemouser"
 }
-
 New-AzureADUser $UserParamameters
 
 # Skapa Resourcegroup --------------------------------------------------
@@ -19,11 +26,10 @@ $location = 'WestEurope'
 New-AzResourceGroup -Name $ResourceGroupName -Location $location
 
 # Assign roles for the user in the resource group ----------------------
-$SubsriptionScope = "/subscriptions/00000000-0000-0000-0000-000000000000"
 $ReaderRoleParameters = @{
     SignInName = $UserPrincipalName;
     RoleDefinitionName = "Reader";
-    Scope = $SubsriptionScope
+    Scope = $SubscriptionScope
 }
 
 $ContributorRoleParameters = @{
