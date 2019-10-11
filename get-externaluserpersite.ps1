8e0a6a5b7c35f2d6866e10bb6760c05e4956acd3
Mats Warnolf <mats@mwab.onmicrosoft.com>
2019-10-11 10:48:45 +0200

    working

--- get-externaluserpersite.ps1
+++ get-externaluserpersite.ps1
@@ -1,23 +1,39 @@
 import-module Microsoft.Online.SharePoint.PowerShell
 $cred = get-credential
-$orgName = "foba"
-$ExternalUsers = @{ }
-Connect-SPOService -Url "https://$orgname-admin.sharepoint.com" -Credential $cred
+[String]$orgName = "foba"
+[System.Array]$ExternalUsers 
+[Int]$sitenumber,$currentuser
+Write-host "Connecting to site https://$orgname-admin.sharepoint.com"
+try {
+    Connect-SPOService -Url "https://$orgname-admin.sharepoint.com" -Credential $cred
+}
+Catch {
+    Write-host "Failed to connect"
+}
+
 $sites = get-sposite -Limit All
+Write-host "found $($sites.count)"
 #get all external users by running
+clear-variable sitenumber,currentuser -ErrorAction SilentlyContinue
 foreach ($site in $sites) {
-    $SiteUsers = get-spoexternaluser -SiteUrl $site.url
+    $sitenumber ++
+write-progress -Activity "processing site $($site.Title)" -Id 1 -PercentComplete ($sitenumber / $sites.Count * 100)
+    $SiteUsers = get-spoexternaluser -SiteUrl $site.url -pagesize 50
     Foreach ($ExternalUser in $SiteUsers) {
-        $Guest = [Ordered] @{
+        $currentuser ++
+        Write-Progress -Activity "processing $($Externaluser.displayname)" -Id 2 -PercentComplete ($currentuser / $siteusers.Count * 100)
+        $Guest = [PSCustomObject] @{
             'URL'         = $Site.URL;
             'Site'        = $Site.Title;
             'DisplayName' = $ExternalUser.DisplayName;
             'Email'       = $ExternalUser.Email;
             'Created'     = $ExternalUser.WhenCreated
         }
-        $GuestUser = New-Object -TypeName psobject -Property $Guest
-        $ExternalUsers.Add("$GuestUser")
+        
+        $ExternalUsers += $guest
     }
-    
+    clear-variable currentuser
+    write-progress -id 2 -Completed -Activity "Done"
 }
+
 
