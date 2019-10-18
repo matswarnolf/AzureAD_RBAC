0b995cc8d79ee14932c2487aa916409f09e55af7
matswarnolf <mats@warnolf.net>
2019-10-18 13:59:11 +0200

    Finally

---
+++ NewRole.ps1
@@ -0,0 +1,4 @@
+Get-AzRoleDefinition -Custom
+New-AzRoleDefinition -InputFile .\CustomRole.json
+Get-AzRoleDefinition -Name "Virtual Machine Operator"
+Get-AzRoleDefinition "Virtual Machine Operator" | Remove-AzRoleDefinition
